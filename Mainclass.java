c0da42a1792ee6264e4e244e8d31069fbbc8cfc7
AnansiyaElizabeth <44959999+AnansiyaElizabeth@users.noreply.github.com>
2018-11-16 11:10:25 +0530

    Add files via upload

---
+++ Mainclass.java
@@ -0,0 +1,17 @@
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+	import com.Employee;
+
+	public class Mainclass {
+	public static void main(String args[])
+	{
+		Resource resource=new ClassPathResource("SpringConfig.xml");
+		BeanFactory factory=new XmlBeanFactory(resource);
+		Employee employee=(Employee) factory.getBean("empbean");
+		System.out.println(employee);
+	}
+	}
+
